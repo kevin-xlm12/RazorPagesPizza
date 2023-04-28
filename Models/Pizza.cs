7b20acad6762ca89a0c85de37d0b6d6ace635acd
kevin-xlm12 <aler8860gmail.com>
2023-04-28 15:07:40 -0300

    mi primer commit 	new file:   obj/Debug/net6.0/RazorPagesPizza.RazorAssemblyInfo.cs

---
+++ Models/Pizza.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Models;
+public class Pizza
+{
+
+    public int Id{get;set;}
+
+    [Required]
+    public string? Name{set;get;}
+    public PizzaSize Size {set;get;}
+    public bool IsGlutenFree {set;get;}
+
+     [Range(0.01, 9999.99)]
+     public decimal Price {set;get;}   
+}
+
+public enum PizzaSize{Small, Medium, Large }
+
+public  class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    public int nextId = 3;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+                {
+                    new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.Large, IsGlutenFree = false },
+                    new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small, IsGlutenFree = true }
+                };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
+}
+
---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
